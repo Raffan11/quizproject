34f811e6a2701747e0f873dcf8c3a161a25b447e
Raffan11 <32261499+Raffan11@users.noreply.github.com>
2017-09-27 14:41:04 +0200

    klar med tutorial!

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from quiz import views
 
 urlpatterns = [
-	path("", views.start),
-	path("quiz/<int:quiz_number>/", views.quiz),
-	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question),
-	path("quiz/<int:quiz_number>/results/", views.results),
+	path("", views.start, name="start_page"),
+	path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
+	path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
+	path("quiz/<int:quiz_number>/results/", views.results, name="results_page"),
 ]
